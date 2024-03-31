b3449995488fdf454fa6362a0e01eb5de40c804e
Khuzin Karim <161218617+SELEYRON@users.noreply.github.com>
2024-03-31 17:08:24 +0300

    script.js 0,2

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ let menu_field = document.querySelector(".menu_field")
 btn_menu.addEventListener("click", function() {
     document.getElementById("menu_field_cont").classList.toggle("show")
 })
-
+btn_games.addEventListener("click", setTimeout(function(){
+    window.location.href="games"}, 5*1000
+))
+btn_applications.addEventListener("click", setTimeout(function(){
+    window.location.href="applications"}, 5*1000
+))
+btn_books.addEventListener("click", setTimeout(function(){
+    window.location.href="books"}, 5*1000
+))
+btn_music.addEventListener("click", setTimeout(function(){
+    window.location.href="music"}, 5*1000
+))
