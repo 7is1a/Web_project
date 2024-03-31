01dbf76a77bdf23f66b8037eeba6cccfd1ca1827
Khuzin Karim <161218617+SELEYRON@users.noreply.github.com>
2024-03-31 16:42:37 +0300

    script.js 0.1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let btn_menu = document.querySelector(".menu")
+let btn_games = document.querySelector(".games")
+let btn_books = document.querySelector(".books")
+let btn_applications = document.querySelector(".applications")
+let btn_music = document.querySelector(".music")
+let menu_field = document.querySelector(".menu_field")
+
+btn_menu.addEventListener("click", function() {
+    document.getElementById("menu_field_cont").classList.toggle("show")
+})
 
